9af0bf4ed310b372139048cca086ddeafdaf18e4
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2024-03-14 18:10:02 +0330

    feat(module:tree-view):init sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,155 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+
+import {NzTreeFlatDataSource, NzTreeFlattener, NzTreeViewModule} from 'ng-zorro-antd/tree-view';
+import {SelectionModel} from "@angular/cdk/collections";
+import {FlatTreeControl} from "@angular/cdk/tree";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+
+interface TreeNode {
+  name: string;
+  disabled?: boolean;
+  children?: TreeNode[];
+}
+
+const TREE_DATA: TreeNode[] = [
+  {
+    name: '0-0',
+    disabled: true,
+    children: [{ name: '0-0-0' }, { name: '0-0-1' }, { name: '0-0-2' }]
+  },
+  {
+    name: '0-1',
+    children: [
+      {
+        name: '0-1-0',
+        children: [{ name: '0-1-0-0' }, { name: '0-1-0-1' }]
+      },
+      {
+        name: '0-1-1',
+        children: [{ name: '0-1-1-0' }, { name: '0-1-1-1' }]
+      }
+    ]
+  }
+];
+
+interface FlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+  disabled: boolean;
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [NzTreeViewModule, NzIconDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+  private transformer = (node: TreeNode, level: number): FlatNode => {
+    const existingNode = this.nestedNodeMap.get(node);
+    const flatNode =
+        existingNode && existingNode.name === node.name
+            ? existingNode
+            : {
+              expandable: !!node.children && node.children.length > 0,
+              name: node.name,
+              level,
+              disabled: !!node.disabled
+            };
+    this.flatNodeMap.set(flatNode, node);
+    this.nestedNodeMap.set(node, flatNode);
+    return flatNode;
+  };
+  flatNodeMap = new Map<FlatNode, TreeNode>();
+  nestedNodeMap = new Map<TreeNode, FlatNode>();
+  checklistSelection = new SelectionModel<FlatNode>(true);
+
+  treeControl = new FlatTreeControl<FlatNode>(
+      node => node.level,
+      node => node.expandable
+  );
+
+  treeFlattener = new NzTreeFlattener(
+      this.transformer,
+      node => node.level,
+      node => node.expandable,
+      node => node.children
+  );
+
+  dataSource = new NzTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.setData(TREE_DATA);
+  }
+
+  hasChild = (_: number, node: FlatNode): boolean => node.expandable;
+
+  descendantsAllSelected(node: FlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    return descendants.length > 0 && descendants.every(child => this.checklistSelection.isSelected(child));
+  }
+
+  descendantsPartiallySelected(node: FlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    const result = descendants.some(child => this.checklistSelection.isSelected(child));
+    return result && !this.descendantsAllSelected(node);
+  }
+
+  leafItemSelectionToggle(node: FlatNode): void {
+    this.checklistSelection.toggle(node);
+    this.checkAllParentsSelection(node);
+  }
+
+  itemSelectionToggle(node: FlatNode): void {
+    this.checklistSelection.toggle(node);
+    const descendants = this.treeControl.getDescendants(node);
+    this.checklistSelection.isSelected(node)
+        ? this.checklistSelection.select(...descendants)
+        : this.checklistSelection.deselect(...descendants);
+
+    descendants.forEach(child => this.checklistSelection.isSelected(child));
+    this.checkAllParentsSelection(node);
+  }
+
+  checkAllParentsSelection(node: FlatNode): void {
+    let parent: FlatNode | null = this.getParentNode(node);
+    while (parent) {
+      this.checkRootNodeSelection(parent);
+      parent = this.getParentNode(parent);
+    }
+  }
+
+  checkRootNodeSelection(node: FlatNode): void {
+    const nodeSelected = this.checklistSelection.isSelected(node);
+    const descendants = this.treeControl.getDescendants(node);
+    const descAllSelected =
+        descendants.length > 0 && descendants.every(child => this.checklistSelection.isSelected(child));
+    if (nodeSelected && !descAllSelected) {
+      this.checklistSelection.deselect(node);
+    } else if (!nodeSelected && descAllSelected) {
+      this.checklistSelection.select(node);
+    }
+  }
+
+  getParentNode(node: FlatNode): FlatNode | null {
+    const currentLevel = node.level;
+
+    if (currentLevel < 1) {
+      return null;
+    }
+
+    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl.dataNodes[i];
+
+      if (currentNode.level < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
 }
